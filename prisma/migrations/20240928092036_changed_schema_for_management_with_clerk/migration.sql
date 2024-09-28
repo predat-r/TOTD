e1f26085d1315093a2be3c187236f687496dc456
predat-r <harisnaeem612@outlook.com>
2024-09-28 14:22:01 +0500

    fixed schema for compatibility with clerk

---
+++ prisma/migrations/20240928092036_changed_schema_for_management_with_clerk/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Thought" DROP CONSTRAINT "Thought_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Like" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Thought" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Thought" ADD CONSTRAINT "Thought_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
