965a0d45049c4399d1f17163cc0409645d007be7
predat-r <harisnaeem612@outlook.com>
2024-09-29 20:03:37 +0500

    added custom text font in globalcss and improved responsiveness

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,10 @@
 function Heading() {
     return (
         <div>
-            <h1 className="text-7xl mb-5 text-center bg-gradient-to-r from-blue-200 to-blue-950 bg-clip-text text-transparent leading-normal">
+            <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-7xl md:h-12 lg:h-24 text-center gradient-text leading-normal">
                 Thought Of The Day
             </h1>
-            <p className="text-center text-xl">Share your thoughts today, gone tomorrow</p>
+            <p className="text-center text-indigo-950  text-xl">Share your thoughts today, gone tomorrow</p>
         </div>
     );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TfiAlignLeft } from "react-icons/tfi";
-
+import Image from "next/image";
 function Navbar() {
   return (
     <>
@@ -9,7 +9,7 @@ function Navbar() {
           <TfiAlignLeft size={30}/>
         </div>
 
-        <div className="text-white font-semibold ml-10 sm:ml-20"><img className="size-12" src="icon.png" alt="logo" /></div>
+        <div className="text-white font-semibold ml-10 sm:ml-20"><Image width={42} height={42} src="/icon.png" alt="logo" /></div>
 
         <div>
           <button className="bg-[#3E0A9E] hover:bg-[#5d1fcf] text-white font-bold py-2 px-4 w-24 md:w-32  rounded-lg">
