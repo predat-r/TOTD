82bb5e5870103cae6dd91b99d96a256346f5a4ee
predat-r <harisnaeem612@outlook.com>
2024-10-02 18:09:24 +0500

    user actions

---
+++ app/lib/userActions.ts
@@ -0,0 +1,21 @@
+'use server';
+import { currentUser } from "@clerk/nextjs/server";
+async function UserActions() {
+  const user = await currentUser();
+  if (!user) {
+    console.error("User not logged in");
+    return;
+  }
+  const username = user.username;
+  const userId = user.id;
+  if (!username) {
+    console.error("Username not found");
+    return;
+  }
+  return {
+    userId,
+    username,
+  };
+}
+
+export default UserActions;
