7219ca0857ffd1cd3c698ef93ff8eb47cd28b250
predat-r <harisnaeem612@outlook.com>
2024-10-11 22:58:09 +0500

    lint errors

--- components/profilePageThoughts.tsx
+++ components/profilePageThoughts.tsx
@@ -11,10 +11,11 @@ const ProfilePageThoughts = async ({username}:{username:string}) => {
     <div>
       <div className="w-[100vw] h-36 flex flex-col  items-center mb-1 ">
         {thoughts !== null
-          ? thoughts.map((element: Thought) => (
-              <div className="w-[100vw] pl-10 h-full flex flex-row flex-shrink-0 items-center justify-center ml-16 gap-x-20">
+          ? thoughts.map((element: Thought ,index) => (
+              <div key={index} className="w-[100vw] pl-10 h-full flex flex-row flex-shrink-0 items-center justify-center ml-16 gap-x-20">
                 <MdOutlineDeleteOutline className="size-7 text-purple-900 hover:text-red-700" />
                 <Card
+                  index={-1}
                   key={element.thoughtId}
                   id={element.thoughtId}
                   text={element.content}
